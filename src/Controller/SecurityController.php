bc083149b551391c799d2651e232e387e96a8729
Fufette <galaffu.dev@gmail.com>
2022-08-17 00:59:31 +0200

    redirect login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -34,7 +34,7 @@ class SecurityController extends AbstractController
     {
         // if user is already logged in, don't display the login page again
         if ($this->getUser()) {
-            return $this->redirectToRoute('blog_index');
+            return $this->redirectToRoute('app_home');
         }
 
         // this statement solves an edge-case: if you change the locale in the login
